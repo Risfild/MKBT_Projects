a4bcd4275b641de0d84b28c93ea8d27952b69b3f
Alex <lapkoaleks@yandex.ru>
2019-04-25 20:06:09 +0300

    bottle homework

---
+++ Bottle/ConsoleApp1/Bottle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Bottle
+    {
+        const int maxVolume = 200;
+        static int volume = 200;
+        public int Volume { get; }
+
+        public Bottle()
+        {
+
+        }
+
+        public void AddVolume(int value)
+        {
+            if(volume/maxVolume*100 >= 95)
+            {
+                Console.WriteLine("Бочка заполнена на {0}% ({1} литров). Вы уверены что хотите продолжить? Да/Нет", volume/maxVolume*100, volume);
+                if (Console.ReadLine().ToLower() == "да")
+                    volume += value;
+                else
+                    return;
+            }
+            else
+            {
+                volume += value;
+            }
+
+            Console.WriteLine("В бочке после наполнения {0} литров", volume);
+        }
+
+        public void MinusVolume(int value)
+        {
+            if(volume - value < 0)
+            {
+                Console.WriteLine("При вычитании {0} литров воды, значение в бочке будет отрицательным. Вы уверены что хотите продолжить? Да/Нет", value);
+                if (Console.ReadLine().ToLower() == "да")
+                    volume -= value;
+                else
+                    return;
+            }
+            else
+                volume -= value;
+
+            Console.WriteLine("В бочке после вычитания {0} литров", volume);
+        }
+    }
+}
---
+++ Bottle/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate void Fill(int value);
+        delegate void Pour(int value);
+        static Bottle bottle = new Bottle();
+        static void Main(string[] args)
+        {
+            Fill FillBottle = bottle.AddVolume;
+            Pour PourBottle = bottle.MinusVolume;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Что вы хотите сделать с бутылкой? \n1. Наполнить \n2. Вылить \nДля выхода из программы введите exit");
+                answer = Console.ReadLine();
+
+                if (answer == "1")
+                {
+                    Console.WriteLine("На сколько вы хотите наполнить бутылку?");
+                    FillBottle(int.Parse(Console.ReadLine()));
+                }
+                else if (answer == "2")
+                {
+                    Console.WriteLine("На сколько вы хотите вылить бутылку?");
+                    PourBottle(int.Parse(Console.ReadLine()));
+                }
+
+            } while (answer != "exit");
+        }
+    }
+}
